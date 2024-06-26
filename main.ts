62bc4797698ef2e1e3b3e51097147823a82c5283
Anabanana0365 <elon.hb0635@gmail.com>
2024-06-26 15:21:06 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,67 @@
+namespace SpriteKind {
+    export const Gas = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(assets.image`Go-N-Fight`, mySprite, 0, -70)
+    projectile.startEffect(effects.fire)
+    music.play(music.melodyPlayable(music.pewPew), music.PlaybackMode.UntilDone)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Projectile, function (sprite, otherSprite) {
+    sprite.destroy(effects.blizzard, 500)
+    otherSprite.destroy(effects.warmRadial, 500)
+    info.changeScoreBy(1)
+})
+info.onScore(100, function () {
+    game.gameOver(true)
+    game.setGameOverEffect(true, effects.confetti)
+    music.play(music.melodyPlayable(music.beamUp), music.PlaybackMode.UntilDone)
+})
+info.onLifeZero(function () {
+    game.gameOver(false)
+    game.setGameOverEffect(false, effects.melt)
+    music.play(music.melodyPlayable(music.wawawawaa), music.PlaybackMode.UntilDone)
+    for (let index = 0; index < 1e+96; index++) {
+        music.stopAllSounds()
+    }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Gas, function (sprite, otherSprite) {
+    statusbar.value = 100
+    otherSprite.destroy()
+})
+statusbars.onZero(StatusBarKind.Energy, function (status) {
+    game.over(false)
+    music.play(music.melodyPlayable(music.wawawawaa), music.PlaybackMode.UntilDone)
+    game.setGameOverEffect(false, effects.melt)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    otherSprite.destroy(effects.halo, 500)
+    music.play(music.melodyPlayable(music.zapped), music.PlaybackMode.UntilDone)
+    info.changeScoreBy(-1)
+})
+let MyEnemy: Sprite = null
+let myFuel: Sprite = null
+let projectile: Sprite = null
+let statusbar: StatusBarSprite = null
+let mySprite: Sprite = null
+info.setScore(0)
+effects.starField.startScreenEffect()
+mySprite = sprites.create(assets.image`FlameShip`, SpriteKind.Player)
+controller.moveSprite(mySprite)
+mySprite.setFlag(SpriteFlag.StayInScreen, true)
+info.changeLifeBy(1)
+statusbar = statusbars.create(20, 4, StatusBarKind.Energy)
+statusbar.attachToSprite(mySprite, -25, 0)
+game.onUpdateInterval(5000, function () {
+    myFuel = sprites.createProjectileFromSide(assets.image`FlameShip-PowerUp`, 0, 50)
+    myFuel.x = randint(5, 155)
+    myFuel.setKind(SpriteKind.Gas)
+})
+game.onUpdateInterval(1000, function () {
+    MyEnemy = sprites.createProjectileFromSide(assets.image`ColdEmenys`, 0, 50)
+    MyEnemy.x = randint(5, 155)
+    MyEnemy.setKind(SpriteKind.Enemy)
+})
+game.onUpdateInterval(300, function () {
+    statusbar.value += -1
+})
